dd2a1507afdbb743f4229bb1368f0714e8a7d24c
salihmusic <salih.music@stu.ssst.edu.ba>
2023-04-04 04:40:14 +0200

    Admin Panel. NavData file

---
+++ src/app/admin-panel/sidenav/nav-data.ts
@@ -0,0 +1,22 @@
+export const navbarData=[
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  },
+  {
+    routeLink: 'rides',
+    icon: 'fal fa-car',
+    label: 'Rides'
+  },
+  {
+    routeLink: 'users',
+    icon: 'fal fa-user',
+    label: 'Users'
+  },
+  {
+    routeLink: 'statistics',
+    icon: 'fal fa-chart-bar',
+    label: 'Statistics'
+  }
+]
