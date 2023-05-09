cd830826182d953e3799ea05ddea12454759475e
kenan <kenan.konjic@stu.ssst.edu.ba>
2023-05-09 11:52:52 +0200

    Removed unnecessary import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateRideComponent } from './create-ride/create-ride.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CreateRideComponent} from './create-ride/create-ride.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from "@angular/material/input";
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 import {MatSelectModule} from '@angular/material/select';
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { SidenavComponent } from './admin-panel/sidenav/sidenav.component';
-import { DashboardComponent } from './admin-panel/dashboard/dashboard.component';
-import { StatisticsComponent } from './admin-panel/statistics/statistics.component';
-import { UsersComponent } from './admin-panel/users/users.component';
-import { RidesComponent } from './admin-panel/rides/rides.component';
-import { HistoryComponent } from './admin-panel/history/history.component';
-import { ChooseRideComponent } from './choose-ride/choose-ride.component';
-import { LoginComponent } from './login/login.component';
+import {AdminPanelComponent} from './admin-panel/admin-panel.component';
+import {SidenavComponent} from './admin-panel/sidenav/sidenav.component';
+import {DashboardComponent} from './admin-panel/dashboard/dashboard.component';
+import {StatisticsComponent} from './admin-panel/statistics/statistics.component';
+import {UsersComponent} from './admin-panel/users/users.component';
+import {RidesComponent} from './admin-panel/rides/rides.component';
+import {HistoryComponent} from './admin-panel/history/history.component';
+import {ChooseRideComponent} from './choose-ride/choose-ride.component';
+import {LoginComponent} from './login/login.component';
 import {UserService} from "./services/user.service";
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
-import { SignupComponent } from './signup/signup.component';
-import {NgOptimizedImage} from "@angular/common";
-import { ChooseRoleComponent } from './choose-role/choose-role.component';
-import { BodyComponent } from './admin-panel/body/body.component';
+import {SignupComponent} from './signup/signup.component';
+import {ChooseRoleComponent} from './choose-role/choose-role.component';
 
 @NgModule({
   declarations: [
@@ -49,7 +47,6 @@ import { BodyComponent } from './admin-panel/body/body.component';
     SignupComponent,
     ChooseRideComponent,
     ChooseRoleComponent,
-    BodyComponent
   ],
   imports: [
     BrowserModule,
