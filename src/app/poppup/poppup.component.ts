c3b73d716862a4249e24350e7e88af74c1f2a7d7
Amra Isakovic <amra,isakovic@stu.ssst.edu.ba>
2023-06-13 00:25:02 +0200

    Fix

--- src/app/poppup/poppup.component.ts
+++ src/app/poppup/poppup.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {PickupModel} from "../models/pickup.model";
+
 
 @Component({
-  selector: 'jw-Poppup',
-  templateUrl: 'Poppup.component.html',
+  selector: 'app-poppup',
+  templateUrl: 'poppup.component.html',
   styleUrls: ['poppup.component.css'],
 })
 export class PoppupComponent implements OnInit {
