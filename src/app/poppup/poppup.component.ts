d901f5ac9166ff978953c3eb8fd006447fbeedee
Amra Isakovic <amra,isakovic@stu.ssst.edu.ba>
2023-06-13 00:09:26 +0200

    Added choose a pickup point page

---
+++ src/app/poppup/poppup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {PickupModel} from "../models/pickup.model";
+
+@Component({
+  selector: 'jw-Poppup',
+  templateUrl: 'Poppup.component.html',
+  styleUrls: ['poppup.component.css'],
+})
+export class PoppupComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+
+}
