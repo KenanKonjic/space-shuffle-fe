0d76945a4c8a6e186e733e6d1dd26708720cd3bb
salihmusic <salih.music@stu.ssst.edu.ba>
2023-06-14 20:22:58 +0200

    UPDATE TO ADMIN
    Now Admin can delete and update changes in Admin Panel Dashboard

--- src/app/admin-panel/dashboard/dashboard.component.ts
+++ src/app/admin-panel/dashboard/dashboard.component.ts
@@ -1,43 +1,97 @@
 import { Component, OnInit } from '@angular/core';
 import { Ride } from '../../models/ride.model';
 import { RideService } from '../../services/ride.service';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
+import { UpdateRideComponent } from './update-ride/update-ride.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { DeleteRideComponent } from './delete-ride/delete-ride.component';
 
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
 }
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent{
+export class DashboardComponent {
   rides: Ride[] = [];
-  displayedColumns: string[] = ['id', 'driverName', 'startingLocation', 'endLocation', 'availableSeats', 'time', 'actions'];
-  displayedColumns2: string[] = ['id', 'driverName', 'startingLocation', 'availableSeats', 'actions'];
-  constructor(private rideService: RideService, private router: Router) { }
+  displayedColumnsDriver: string[] = [
+    'id',
+    'driverName',
+    'startingLocation',
+    'endLocation',
+    'availableSeats',
+    'time',
+    'actions',
+  ];
+  displayedColumnsUser: string[] = [
+    'id',
+    'driverName',
+    'startingLocation',
+    'availableSeats',
+    'actions',
+  ];
+  constructor(
+    private rideService: RideService,
+    private router: Router,
+    public dialog: MatDialog,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-    this.rideService.getRides().subscribe((rides)=> {
-      this.rides=rides;
-      console.log(rides);
+    this.setRides();
+  }
+
+  setRides(): void {
+    this.rideService.getRides().subscribe((rides) => {
+      this.rides = rides;
     });
   }
 
-
-
   selectRide(ride: Ride) {
-    this.router.navigate(['home']);
+    const dialogRef = this.dialog.open(UpdateRideComponent, {
+      width: '400px',
+      data: ride,
+    });
+
+    dialogRef.afterClosed().subscribe((ride: Ride) => {
+      if (ride) {
+        this.rideService.updateRide(ride).subscribe(
+          (res) => {
+            this.toastr.success(`Ride with id ${res.id} successfully updated!`);
+            this.setRides();
+          },
+          (error) => {
+            this.toastr.error(`Ride with id ${ride.id} could not be updated!`);
+          }
+        );
+      }
+    });
   }
 
-  title='sidenav';
+  deleteRide(rideId: number): void {
+    const dialogRef = this.dialog.open(DeleteRideComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(rideId);
+      if (result === 'delete') {
+        this.rideService.deleteRide(rideId).subscribe((res) => {
+          this.toastr.success(`Ride with id ${rideId} successfully deleted!`);
+          this.setRides();
+        });
+      }
+    });
+  }
+
+  title = 'sidenav';
   isSideNavCollapsed = false;
-  screenWidth= 0;
-  onToggleSideNav(data: SideNavToggle): void {
-    this.screenWidth=data.screenWidth;
-    this.isSideNavCollapsed=data.collapsed;
-  }
+  screenWidth = 0;
 
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
